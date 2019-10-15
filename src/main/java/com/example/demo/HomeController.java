ab0f7cb53e55191e1d2f694df015f19c13e8d6eb
sbijukchhe <skbijukchhe@gmail.com>
2019-10-15 16:42:18 -0400

    Persisting Current User Information

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model) {
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
+    }
+
+}
---
+++ src/main/java/com/example/demo/SpringBoot405PersistingUserInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot405PersistingUserInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot405PersistingUserInfoApplication.class, args);
+    }
+
+}
